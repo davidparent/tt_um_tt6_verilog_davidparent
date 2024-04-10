eff2594172b32b92132bb055c77b180a601208a4
davidparent <david.parent@sjsu.edu>
2024-04-10 06:43:29 -0700

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -5,19 +5,19 @@ module QIFNeuron (
   output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
   
-  wire reset = ! rst_n;
+   
   reg [7:0] A;  
-  assign V_mem =I_syn;
+  assign V_mem=A;
   always @(posedge clk  or posedge rst_n) begin
-        // if reset, set counter to 0
+          
         if (rst_n) begin
             A <= 0;
-        end else begin
+       end else begin
             if (A>=8'sd50) begin
                 A<=-8'sd20;
             end else begin
                A<=A+I_syn/4+(A/8)*(A/8);
         end
         end
-    end
+   end
 endmodule
