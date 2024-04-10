336127b5a0a406fbdba538db900130a63c1e20d8
davidparent <david.parent@sjsu.edu>
2024-04-10 06:40:01 -0700

    Update QIFNeuron.v

--- src/QIFNeuron.v
+++ src/QIFNeuron.v
@@ -1,29 +1,23 @@
 module QIFNeuron (
   input wire clk,          // Clock input
-  input wire rst_n,       // Reset input
-  input   wire [7:0] I_syn,     // Input B (8-bit, signed)
+  input  wire rst_n,       // Reset input
+  input  wire [7:0] I_syn,     // Input B (8-bit, signed)
   output  wire [7:0] V_mem     // Output voltage V (8-bit, signed) 
 );
-  reg [7:0] V;
-  reg [7:0] B;
-  //wire reset = ! rst_n;
-   //reg [7:0] Z1, Z2;        // Delay flip-flops
-  //wire [7:0] V_reset = -8'sd20; // Set Vreset to -20
-  //wire [7:0] Vpeak = 8'd50;  // Vpeak value (Set to 50)
-  //wire [7:0] A = 8'd32;      // Gain A (Set to 0.25)
   
-  assign V_mem = V;
-  
-  always @(posedge clk) begin
+  wire reset = ! rst_n;
+  reg [7:0] A;  
+  assign V_mem =I_syn;
+  always @(posedge clk  or posedge rst_n) begin
         // if reset, set counter to 0
-      if (rst_n) begin
-            V <= 0;
-            //B <=0 ;
-     
+        if (rst_n) begin
+            A <= 0;
         end else begin
-            V <= I_syn;
-            //B <=1 ;
-      
+            if (A>=8'sd50) begin
+                A<=-8'sd20;
+            end else begin
+               A<=A+I_syn/4+(A/8)*(A/8);
         end
-  end
+        end
+    end
 endmodule
